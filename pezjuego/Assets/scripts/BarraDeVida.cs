f504200b2e2badf0d90dec1c5199f9c3482951d7
sssrcv <146860093+sssccv@users.noreply.github.com>
2024-05-01 20:44:01 -0600

    Agregué el método para sumar y restar vida
    lo que dice arriba

--- pezjuego/Assets/scripts/BarraDeVida.cs
+++ pezjuego/Assets/scripts/BarraDeVida.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BarraDeVida : MonoBehaviour
 {
-    public Image barraDeVida;
-    public float vidaActual;
-    public float vidaMáxima;
+    public Image barradevida;
+    public float vidaactual;
+    public float vidamaxima;
+    public float modvida;
     void Update()
     {
-        barraDeVida.fillAmount = vidaActual / vidaMáxima;
+        barradevida.fillAmount = vidaactual/vidamaxima;
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            vidaactual = vidaactual-modvida;
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            vidaactual = vidaactual + modvida;
+        }
+        if(vidaactual>=vidamaxima)
+        {
+            vidaactual = vidamaxima;
+        }
+        if (vidaactual <= 0)
+        {
+            vidaactual = 0;
+        }
     }
+    
 }
