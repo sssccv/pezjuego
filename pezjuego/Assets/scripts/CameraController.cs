adb6fa3a236382c26d4240e7f16fd069ce362145
Copycat339 <161411275+Copycat339@users.noreply.github.com>
2024-05-23 09:51:01 -0600

    Muelle
    si

--- pezjuego/Assets/scripts/CameraController.cs
+++ pezjuego/Assets/scripts/CameraController.cs
@@ -7,10 +7,11 @@ public class CameraController : MonoBehaviour
     public Transform[] waypoints; // Los puntos a los que la cámara se moverá
     public float transitionTime = 0.5f; // Tiempo de transición entre puntos
     public Transform target1; // El primer objetivo (personaje) al que la cámara debe mirar
-   
+
     private int currentWaypointIndex = 0;
     private float transitionTimer = 0f;
     private bool isLookingAtFirstTarget = true; // Indica si la cámara está mirando al primer objetivo
+    private Vector3 velocity = Vector3.zero; // Velocidad utilizada por SmoothDamp
 
     void Update()
     {
@@ -22,17 +23,15 @@ public class CameraController : MonoBehaviour
         {
             currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
             transitionTimer = 0f;
-            
         }
 
-        // Mueve la cámara hacia el punto actual
-        transform.position = Vector3.Lerp(transform.position, waypoints[currentWaypointIndex].position, transitionTimer / transitionTime);
+        // Mueve la cámara hacia el punto actual utilizando SmoothDamp
+        transform.position = Vector3.SmoothDamp(transform.position, waypoints[currentWaypointIndex].position, ref velocity, transitionTime);
 
         // Asegura que la cámara mire hacia el objetivo correspondiente
         if (isLookingAtFirstTarget)
         {
             transform.LookAt(target1);
         }
-       
     }
 }
